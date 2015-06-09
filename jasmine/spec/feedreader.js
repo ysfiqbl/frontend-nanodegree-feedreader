cde2202a044415d40163e5945a58549ad138fe33
Yusuf Iqbal <ysf.iqbl@gmail.com>
2015-06-09 22:52:09 +0530

    Added two tests to RSS Feed test suite.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,16 +27,28 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('has a URL and the URL not empty', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe('');
+            }
+        })
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('has a name and the name not empty', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBe('');
+            }
+        })
     });
 
 
